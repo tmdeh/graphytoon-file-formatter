7d45c5c739a6a70ab7b821efc67aeb2cec18e75b
유승도 <yus174113@gmail.com>
2022-10-20 20:17:52 +0900

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+let files = fs.readdirSync('folder');
+
+function fillZero(width, str){
+  return str.length >= width ? str:new Array(width-str.length+1).join('0')+str;
+}
+
+
+let duplicatedListFile = fs.readFileSync('list.txt', {encoding: 'utf-8'})
+
+let duplicated = duplicatedListFile.trim()
+
+
+duplicated = duplicatedListFile.split('\n').map(v => {
+  return v.trim()
+})
+
+// console.log(duplicated)
+
+let duplicatedMap = {}
+duplicated.forEach(file => {
+  duplicatedMap[file]=true;
+})
+
+
+files.forEach(file => {
+  let [filename, ext] = file.split('.');
+
+  const [shape, filenumber] = filename.split('_')
+  filename = `shape.${shape}.${fillZero(3, filenumber)}.${ext}`
+  console.log(filename)
+
+  if(duplicatedMap[filename]){
+  }else{
+    fs.copyFile('./folder/' + file ,'./result/' + filename, function(err){
+    if(err) console.log('something wrong was happened')
+    else console.log('copy file succeed');
+    })
+    duplicatedListFile += filename + "\n";
+  }
+})
+
+fs.writeFile('new.txt', duplicatedListFile, (err)=> {
+  if(err) {
+    console.error(err);
+  }
+})
+
